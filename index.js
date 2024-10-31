fff1b4aafe131d09b262f5ec233d1289a35028b8
K.M.I.I.Madhushani <139638076+ImeshaMadhushani@users.noreply.github.com>
2024-10-31 08:33:23 +0530

    chage bort back

--- index.js
+++ index.js
@@ -66,6 +66,6 @@ mongoose.connect(connectionString).then(
     } 
     )
 
-app.listen(4000, (req, res) => {
-    console.log('Server is running on port 4000');
+app.listen(5000, (req, res) => {
+    console.log('Server is running on port 5000');
 })
